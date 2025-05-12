ad8eea7798c0ef5bd20079012ba1b8252f0b1e13
Nathan <viniciosnathan@gmail.com>
2025-05-12 15:17:54 -0300

    feat: implement UserController with CRUD operations for user management

---
+++ backend/src/controllers/users/user-controller.ts
@@ -0,0 +1,35 @@
+import { FastifyRequest, FastifyReply } from 'fastify'
+import { UserService } from '../../services/users/user-service'
+import { CreateUserDTO, UpdateUserDTO } from '../../types/users/user-types'
+
+export class UserController {
+  constructor(private userService = new UserService()) {}
+
+  async create(request: FastifyRequest<{ Body: CreateUserDTO }>, reply: FastifyReply) {
+    const user = await this.userService.create(request.body)
+    return reply.status(201).send(user)
+  }
+
+  async list(request: FastifyRequest, reply: FastifyReply) {
+    const users = await this.userService.list()
+    return reply.send(users)
+  }
+
+  async findById(request: FastifyRequest<{ Params: { id: string } }>, reply: FastifyReply) {
+    const user = await this.userService.findById(request.params.id)
+    return reply.send(user)
+  }
+
+  async update(
+    request: FastifyRequest<{ Params: { id: string }, Body: UpdateUserDTO }>,
+    reply: FastifyReply
+  ) {
+    const user = await this.userService.update(request.params.id, request.body)
+    return reply.send(user)
+  }
+
+  async delete(request: FastifyRequest<{ Params: { id: string } }>, reply: FastifyReply) {
+    await this.userService.delete(request.params.id)
+    return reply.status(204).send()
+  }
+}
