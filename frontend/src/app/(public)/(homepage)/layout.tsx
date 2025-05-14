412e5ac47a420d4acb3a383ee956126e4a48d7be
Nathan <viniciosnathan@gmail.com>
2025-05-14 13:41:55 -0300

    fix: run eslint --fix

--- frontend/src/app/(public)/(homepage)/layout.tsx
+++ frontend/src/app/(public)/(homepage)/layout.tsx
@@ -1,5 +1,7 @@
-import { Metadata } from "next";
+import type { Metadata } from "next";
+
 import Header from "@/app/components/Header";
+
 import "../../globals.css";
 
 export const metadata: Metadata = {
--- frontend/src/app/(public)/(homepage)/page.tsx
+++ frontend/src/app/(public)/(homepage)/page.tsx
@@ -1,12 +1,12 @@
-import React from 'react'
+import React from "react";
 
-const Homepage = () => {
+function Homepage() {
   return (
     <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100">
       <h1 className="text-4xl font-bold text-gray-800">Welcome to GlossaryUP</h1>
-      
+
     </div>
-  )
+  );
 }
 
-export default Homepage
+export default Homepage;
