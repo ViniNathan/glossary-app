a595f34e3fa070dd715e9fe19ce1210eda453371
Nathan <viniciosnathan@gmail.com>
2025-05-14 19:09:13 -0300

    feat: add FeaturesGrid component to Homepage for enhanced content display

--- frontend/src/app/(public)/(homepage)/page.tsx
+++ frontend/src/app/(public)/(homepage)/page.tsx
@@ -1,10 +1,12 @@
 import React from "react";
 import HeroSection from "@/app/components/HeroSection";
+import FeaturesGrid from "@/app/components/FeaturesGrid";
 
 function Homepage() {
   return (
     <div className="">
       <HeroSection />
+      <FeaturesGrid />
     </div>
   );
 }
