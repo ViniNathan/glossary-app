a4e501d826b7744bbc237b834a46a027d28d7943
Nathan <viniciosnathan@gmail.com>
2025-05-21 10:16:12 -0300

    refact: improve code formatting and consistency in FeaturesGrid component

--- frontend/src/app/components/FeaturesGrid.tsx
+++ frontend/src/app/components/FeaturesGrid.tsx
@@ -1,12 +1,13 @@
-import React from 'react'
-import Image from 'next/image'
-import rocketship from '../../assets/rocketship.png'
-import gamification from '../../assets/gamification.png'
-import ranking from '../../assets/ranking.png'
-import smartphone from '../../assets/smartphone.png'
-import { s } from 'motion/react-client'
+import { s } from "motion/react-client";
+import Image from "next/image";
+import React from "react";
 
-const FeaturesGrid = () => {
+import gamification from "../../assets/gamification.png";
+import ranking from "../../assets/ranking.png";
+import rocketship from "../../assets/rocketship.png";
+import smartphone from "../../assets/smartphone.png";
+
+function FeaturesGrid() {
   return (
     <>
       <h1 className="text-4xl md:text-6xl font-bold text-center my-8">Como funciona</h1>
@@ -14,7 +15,11 @@ const FeaturesGrid = () => {
         {/* Card 1 */}
         <div className="rounded-2xl bg-gradient-to-br from-purple-700 to-violet-900 flex flex-col justify-end p-6 md:p-8 shadow-lg h-[300px] md:h-auto lg:col-span-3 lg:row-span-2">
           <h2 className="text-xl md:text-2xl font-bold text-white mb-4">Aprenda novas palavras</h2>
-          <p className="text-white text-sm opacity-80">Aprenda novas palavras<br />com o nosso sistema de gamificação.</p>
+          <p className="text-white text-sm opacity-80">
+            Aprenda novas palavras
+            <br />
+            com o nosso sistema de gamificação.
+          </p>
           <button className="mt-2 px-6 py-2 bg-white text-purple-700 rounded-full font-semibold shadow">Começar agora</button>
         </div>
 
@@ -23,9 +28,9 @@ const FeaturesGrid = () => {
           <div className="absolute inset-0 z-10 p-6">
             <h2 className="text-xl md:text-2xl font-bold text-white mb-4">Evolua na velocidade da luz</h2>
           </div>
-          <Image 
-            src={rocketship} 
-            alt="Rocketship" 
+          <Image
+            src={rocketship}
+            alt="Rocketship"
             className="object-cover opacity-85"
             fill={true}
             sizes="(max-width: 768px) 100vw, 33vw"
@@ -36,27 +41,31 @@ const FeaturesGrid = () => {
         {/* Card 3 */}
         <div className="rounded-2xl bg-gradient-to-tr from-blue-800 to-violet-950 flex flex-col justify-end items-center p-6 shadow-lg h-[300px] relative overflow-hidden md:h-auto lg:col-span-1 lg:row-span-4">
           <h2 className="text-xl md:text-2xl font-bold text-white mb-2 z-10">Interface minimalista</h2>
-          <Image 
-            src={smartphone} 
-            alt="Smartphone" 
+          <Image
+            src={smartphone}
+            alt="Smartphone"
             className="object-contain"
             fill={true}
             sizes="(max-width: 768px) 100vw, 33vw"
             priority
           />
-          <p className="text-white text-sm z-10">Obtenha a alta performance<br />com uma estrutura moderna.</p>
+          <p className="text-white text-sm z-10">
+            Obtenha a alta performance
+            <br />
+            com uma estrutura moderna.
+          </p>
         </div>
 
         {/* Card 4 */}
         <div className="rounded-2xl bg-black flex flex-col justify-between items-start p-6 md:p-8 shadow-lg h-[300px] md:h-auto lg:col-span-2 lg:row-span-2 relative overflow-hidden">
           <h2 className="text-xl md:text-2xl font-bold text-white mb-4 z-10">Gamificação</h2>
-          <Image 
-              src={gamification} 
-              alt="Gamification" 
-              className="object-contain object-bottom-right opacity-35"
-              fill={true}
-              sizes="(max-width: 768px) 100vw, 33vw"
-              priority
+          <Image
+            src={gamification}
+            alt="Gamification"
+            className="object-contain object-bottom-right opacity-35"
+            fill={true}
+            sizes="(max-width: 768px) 100vw, 33vw"
+            priority
           />
           <div className="w-full z-10">
             <p className="text-4xl md:text-sm">Sistema de vidas, XP e ranking</p>
@@ -66,9 +75,9 @@ const FeaturesGrid = () => {
         {/* Card 5 */}
         <div className="rounded-2xl bg-gradient-to-br from-purple-950 to-black flex flex-col justify-between items-start relative overflow-hidden p-6 md:p-8 shadow-lg h-[300px] md:h-auto md:col-span-2 lg:col-span-3 lg:row-span-2">
           <h2 className="text-xl md:text-2xl font-bold text-white mb-4 z-10">Histórico e estatísticas</h2>
-          <Image 
-            src={ranking} 
-            alt="Ranking" 
+          <Image
+            src={ranking}
+            alt="Ranking"
             className="object-contain object-bottom-right opacity-15"
             fill={true}
             sizes="(max-width: 768px) 100vw, 33vw"
@@ -78,7 +87,7 @@ const FeaturesGrid = () => {
         </div>
       </div>
     </>
-  )
+  );
 }
 
-export default FeaturesGrid
+export default FeaturesGrid;
