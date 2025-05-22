6c13309883651fcad41c0965c7f8f42ca7e9504b
Nathan <viniciosnathan@gmail.com>
2025-05-22 13:16:29 -0300

    style: adjust width of stats container for better layout on small devices

--- frontend/src/app/components/HomepageCard.tsx
+++ frontend/src/app/components/HomepageCard.tsx
@@ -27,7 +27,7 @@ function HomepageCard() {
             <h1 className="text-2xl font-semibold">Expanda seu vocabulário</h1>
             <p className="text-lg text-gray-400">Quanto mais você pratica, mais rápido expande seu vocabulário em inglês. Aumente suas habilidades linguísticas dia após dia.</p>
           </div>
-          <div className="md:w-1/2 flex flex-row justify-between items-center">
+          <div className="w-9/10 md:w-1/2 flex flex-row justify-between items-center">
             {stats.map(stat => (
               <div key={stat.id} className="flex flex-col justify-center items-center">
                 <h1 className="text-4xl font-bold text-[#7f62f4]">{stat.value}</h1>
