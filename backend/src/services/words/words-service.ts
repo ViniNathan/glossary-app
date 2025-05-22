da59aced31aa51340578e39b779b934ddeb3b156
Nathan <viniciosnathan@gmail.com>
2025-05-22 09:05:55 -0300

    feat: implement WordService for managing word operations

---
+++ backend/src/services/words/words-service.ts
@@ -0,0 +1,46 @@
+import { WordRepository } from "../../repositories/words/word-repository";
+import { CreateWordDTO, UpdateWordDTO } from "../../types/words/word-types";
+import { AppError } from "../../utils/errors/app-error";
+
+export class WordService {
+    constructor(private wordRepository = new WordRepository()) { }
+    
+    async create(data: CreateWordDTO) {
+        const wordExists = await this.wordRepository.findByWord(data.english_word);
+        if (wordExists) {
+            throw new AppError("Palavra já cadastrada", 400);
+        }
+        return this.wordRepository.create(data);
+    }
+
+    async list() {
+        return this.wordRepository.list();
+    }
+
+    async findById(id: string) {
+        const word = await this.wordRepository.findById(id);
+        if (!word) {
+            throw new AppError("Palavra não encontrada", 404);
+        }
+        return word;
+    }
+    
+    async findByWord(word: string) {
+        const wordExists = await this.wordRepository.findByWord(word);
+        if (!wordExists) {
+            throw new AppError("Palavra não encontrada", 404);
+        }
+        return wordExists;
+    }
+
+    async update(id: string, data: UpdateWordDTO) {
+        await this.findById(id);
+        return this.wordRepository.update(id, data);
+    }
+
+    async delete(id: string) {
+        await this.findById(id);
+        return this.wordRepository.delete(id);
+    }
+
+}
