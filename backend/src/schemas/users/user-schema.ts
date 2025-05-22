9b0d4df24c1681f92495383830dae857166e9ed9
Gabriel Castilho <g248291@dac.unicamp.br>
2025-05-22 11:39:22 -0300

    feat: user schema with zod

--- backend/src/schemas/users/user-schema.ts
+++ backend/src/schemas/users/user-schema.ts
@@ -1,31 +1,31 @@
-import { Type } from "@fastify/type-provider-typebox";
+import { z } from "zod/v4";
 
 export const createUserSchema = {
-  body: Type.Object({
-    name: Type.String(),
-    email: Type.String({ format: "email" }),
-    password: Type.String({ minLength: 6 }),
+  body: z.object({
+    name: z.string(),
+    email: z.email(),
+    password: z.string().min(6),
   }),
   response: {
-    201: Type.Object({
-      id: Type.String(),
-      name: Type.String(),
-      email: Type.String(),
+    201: z.object({
+      id: z.string(),
+      name: z.string(),
+      email: z.string(),
     }),
   },
 };
 
 export const updateUserSchema = {
-  body: Type.Object({
-    name: Type.Optional(Type.String()),
-    email: Type.Optional(Type.String({ format: "email" })),
-    password: Type.Optional(Type.String({ minLength: 6 })),
+  body: z.object({
+    name: z.optional(z.string()),
+    email: z.optional(z.email()),
+    password: z.optional(z.string().min(6)),
   }),
   response: {
-    200: Type.Object({
-      id: Type.String(),
-      name: Type.String(),
-      email: Type.String(),
+    200: z.object({
+      id: z.string(),
+      name: z.string(),
+      email: z.string(),
     }),
   },
 };
