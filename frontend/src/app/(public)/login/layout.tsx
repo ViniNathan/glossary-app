e293e3b41c983eb211ed38d25235a8971fb35728
Nathan <viniciosnathan@gmail.com>
2025-05-23 07:45:52 -0300

    feat: add metadata for login page layout

---
+++ frontend/src/app/(public)/login/layout.tsx
@@ -0,0 +1,8 @@
+import type { Metadata } from "next";
+
+import "@/app/globals.css";
+
+export const metadata: Metadata = {
+  title: "GlossaryUP - Login",
+  description: "GlossaryUP is a platform for students to refine their skills and knowledge.",
+};
