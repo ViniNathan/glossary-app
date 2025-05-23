0fa6cd5ce2a2c199e2a9455d2fd41b38317e8324
Nathan <viniciosnathan@gmail.com>
2025-05-23 08:04:33 -0300

    style: update login card background gradient and submit button color

--- frontend/src/app/(public)/login/page.tsx
+++ frontend/src/app/(public)/login/page.tsx
@@ -4,7 +4,7 @@ function LoginPage() {
   return (
     <div className="container mx-auto flex h-screen flex-col items-center justify-center gap-8">
       <h1 className="text-4xl font-bold">Comece sua jornada</h1>
-      <div className="w-max flex flex-col items-left justify-center border border-gray-600 rounded-lg p-4">
+      <div className="w-max flex flex-col items-left justify-center bg-gradient-to-br from-[#17161d] to-[#121213] border border-gray-600 rounded-lg p-4">
         <h1 className="text-2xl font-bold mb-4">Entrar</h1>
         <p>Faça login para começar sua jornada de aprendizado</p>
 
@@ -21,7 +21,7 @@ function LoginPage() {
           />
           <button
             type="submit"
-            className="bg-[#7f62f4] text-white rounded-lg p-2"
+            className="bg-[#8b72ec] text-white rounded-lg p-2"
           >
             Entrar
           </button>
