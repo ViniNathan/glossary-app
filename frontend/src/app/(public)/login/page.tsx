3cd09ee19582ee5087e91a5afbcee563bc0d8861
Nathan <viniciosnathan@gmail.com>
2025-05-23 08:36:56 -0300

    style: adjust paragraph text size for small devices

--- frontend/src/app/(public)/login/page.tsx
+++ frontend/src/app/(public)/login/page.tsx
@@ -17,7 +17,7 @@ function LoginPage() {
       </h1>
       <div className="w-max flex flex-col items-left justify-center bg-gradient-to-br from-[#17161d] to-[#121213] border border-gray-600 rounded-lg px-6 py-10">
         <h1 className="text-lg md:text-2xl font-bold">Entrar</h1>
-        <p className="text-md text-gray-400">Faça login para começar sua jornada de aprendizado</p>
+        <p className="text-sm md:text-md text-gray-400">Faça login para começar sua jornada de aprendizado</p>
 
         <form className="flex flex-col gap-4 mt-8">
           <label htmlFor="email" className="text-sm font-semibold mb-[-0.5rem]">Email</label>
