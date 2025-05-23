0af85ed3d05d5aafb695b347cd021aefcb0a64e8
Nathan <viniciosnathan@gmail.com>
2025-05-23 08:01:32 -0300

    fix: adjust items display of login card items to left alignment

--- frontend/src/app/(public)/login/page.tsx
+++ frontend/src/app/(public)/login/page.tsx
@@ -4,7 +4,7 @@ function LoginPage() {
   return (
     <div className="container mx-auto flex h-screen flex-col items-center justify-center gap-8">
       <h1 className="text-4xl font-bold">Comece sua jornada</h1>
-      <div className="w-max flex flex-col items-center justify-center border border-gray-600 rounded-lg p-4">
+      <div className="w-max flex flex-col items-left justify-center border border-gray-600 rounded-lg p-4">
         <h1 className="text-2xl font-bold mb-4">Entrar</h1>
         <p>Faça login para começar sua jornada de aprendizado</p>
 
