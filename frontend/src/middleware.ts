0a14a81f836aa3b283e92c9c6aca4e3adfacb199
Nathan <viniciosnathan@gmail.com>
2025-05-27 12:56:50 -0300

    feat: add middleware for route protection and authentication handling

---
+++ frontend/src/middleware.ts
@@ -0,0 +1,54 @@
+import { NextResponse } from "next/server";
+
+const publicRoutes = [
+  { path: "/login", whenAuthenticated: "redirect" },
+  { path: "/register", whenAuthenticated: "redirect" },
+  { path: "/", whenAuthenticated: "next" },
+];
+
+const REDIRECT_WHEN_NOT_AUTHENTICADED_ROUTE = "/login";
+
+export function middleware(request: any) {
+  const requestPath = request.nextUrl.pathname;
+  const PublicRoute = publicRoutes.find(route => route.path === requestPath);
+  const authToken = request.localStorage?.getItem("glossaryUpToken") || request.cookies.get("glossaryUpToken")?.value;
+
+  if (!authToken && PublicRoute) {
+    return NextResponse.next();
+  }
+
+  if (!authToken && !PublicRoute) {
+    const redirectUrl = request.nextUrl.clone();
+    redirectUrl.pathname = REDIRECT_WHEN_NOT_AUTHENTICADED_ROUTE;
+
+    return NextResponse.redirect(redirectUrl);
+  }
+
+  if (authToken && PublicRoute && PublicRoute.whenAuthenticated === "redirect") {
+    const redirectUrl = request.nextUrl.clone();
+    redirectUrl.pathname = "/dashboard";
+
+    return NextResponse.redirect(redirectUrl);
+  }
+
+  if (authToken && !PublicRoute) {
+    // Check if JWT token isn't expired
+    // If it is, redirect to login page
+    return NextResponse.next();
+  }
+
+  return NextResponse.next();
+};
+
+export const config = {
+  matcher: [
+    /*
+         * Match all request paths except for the ones starting with:
+         * - api (API routes)
+         * - _next/static (static files)
+         * - _next/image (image optimization files)
+         * - favicon.ico, sitemap.xml, robots.txt (metadata files)
+         */
+    "/((?!api|_next/static|_next/image|favicon.ico|sitemap.xml|robots.txt).*)",
+  ],
+};
