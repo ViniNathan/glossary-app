cf049f8b0f6bfcc6a64eefc48e174be6676db4a0
Nathan <viniciosnathan@gmail.com>
2025-05-28 09:23:08 -0300

    feat: add Word component to Dashboard for displaying a title

---
+++ frontend/src/app/components/Dashboard/Word.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Word() {
+  return (
+    <div className="flex flex-col justify-center items-center w-full max-w-2xl">
+      <h2 className="text-6xl font-semibold">Word</h2>
+    </div>
+  );
+}
+
+export default Word;
