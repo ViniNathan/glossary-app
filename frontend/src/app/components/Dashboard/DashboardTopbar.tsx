8c120080733073742d8543ff80c33415a4f17387
Nathan <viniciosnathan@gmail.com>
2025-05-28 09:43:00 -0300

    fix: adjust margin-top for small devices in DashboardTopbar component

--- frontend/src/app/components/Dashboard/DashboardTopbar.tsx
+++ frontend/src/app/components/Dashboard/DashboardTopbar.tsx
@@ -3,7 +3,7 @@ import { FaHeart } from "react-icons/fa6";
 
 function DashboardTopbar() {
   return (
-    <div className="container mt-10 mx-auto px-4 sm:px-6 lg:px-8">
+    <div className="container mt-4 md:mt-10 mx-auto px-4 sm:px-6 lg:px-8">
       <div className="flex flex-row justify-center items-center gap-4">
         <div className="bg-gray-500 h-6 w-full rounded-2xl"></div>
         <div className="text-red-400 flex flex-row justify-center items-center gap-2">
