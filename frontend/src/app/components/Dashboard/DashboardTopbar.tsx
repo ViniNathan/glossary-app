ab79760c7abbe0438d7e3d81194e9401f0a82f7b
Nathan <viniciosnathan@gmail.com>
2025-05-28 09:23:38 -0300

    feat: add DashboardTopbar component for displaying timeout and lives

---
+++ frontend/src/app/components/Dashboard/DashboardTopbar.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+import { FaHeart } from "react-icons/fa6";
+
+function DashboardTopbar() {
+  return (
+    <div className="container mt-10 mx-auto px-4 sm:px-6 lg:px-8">
+      <div className="flex flex-row justify-center items-center gap-4">
+        <div className="bg-gray-500 h-6 w-full rounded-2xl"></div>
+        <div className="text-red-400 flex flex-row justify-center items-center gap-2">
+          <FaHeart className="size-6 md:size-8" />
+          <span className="text-2xl font-semibold">5</span>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default DashboardTopbar;
