64622b82f0c46afaca41518ba239630bf5413498
Nathan <viniciosnathan@gmail.com>
2025-05-30 13:55:38 -0300

    feat: create UpdateUserDTO interface

--- backend/src/types/users/user-types.ts
+++ backend/src/types/users/user-types.ts
@@ -7,9 +7,14 @@ export interface CreateUserDTO {
 }
 
 export interface UpdateUserDTO {
-  name?: string;
-  email?: string;
-  password?: string;
+  name: string;
+  email: string;
+  password: string;
+  newPassword?: string;
+  confirmNewPassword?: string;
+  newName?: string;
+  newEmail?: string;
+  confirmNewEmail?: string;
 }
 
 export interface User {
