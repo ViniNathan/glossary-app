9cf75d6dedc4a569c548494eda6610d8b4eab787
Nathan <viniciosnathan@gmail.com>
2025-06-04 19:51:18 -0300

    feat: add schemas to word routes for improved validation

--- backend/src/routes/words/index.ts
+++ backend/src/routes/words/index.ts
@@ -1,7 +1,7 @@
 import type { FastifyInstance } from "fastify/fastify";
 
 import { WordController } from "../../controllers/words/word-controller";
-import { createWordSchema, getRandomWordSchema, updateWordSchema } from "../../schemas/words/word-schema";
+import { createWordSchema, deleteWordSchema, findWordByIdSchema, findWordByWordSchema, getRandomWordSchema, listWordsSchema, updateWordSchema } from "../../schemas/words/word-schema";
 
 export async function wordRoutes(app: FastifyInstance) {
   const wordController = new WordController();
@@ -10,17 +10,25 @@ export async function wordRoutes(app: FastifyInstance) {
     schema: createWordSchema,
   }, wordController.create);
 
-  app.get("/words", wordController.list);
+  app.get("/words", {
+    schema: listWordsSchema,
+  }, wordController.list);
 
-  app.get("/words/search/by-id/:id", wordController.findById);
+  app.get("/words/search/by-id/:id", {
+    schema: findWordByIdSchema,
+  }, wordController.findById);
 
-  app.get("/words/search/by-word/:word", wordController.findByWord);
+  app.get("/words/search/by-word/:word", {
+    schema: findWordByWordSchema,
+  }, wordController.findByWord);
 
   app.put("/words/:id", {
     schema: updateWordSchema,
   }, wordController.update);
 
-  app.delete("/words/:id", wordController.delete);
+  app.delete("/words/:id", {
+    schema: deleteWordSchema,
+  }, wordController.delete);
 
   app.get("/words/random", {
     schema: getRandomWordSchema,
